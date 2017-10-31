822558c400bbec444d401150b1948b4e655ec47d
SU-RU <oooaa0727@gmail.com>
2017-10-31 22:09:18 +0800

    error 練習 2-3：改用 new 的方式新增資料

--- app/Post.php
+++ app/Post.php
@@ -6,11 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected$table='posts';
-    protected$fillable=[
+    protected $table='posts';
+    protected $fillable=[
         'title',
         'content',
         'is_feature'
     ];
 
 }
+/*
+$post	=	new	\App\Post();
+$post->title	=	'test	title';
+$post->content	=	'test	content';
+$post->save();
+*/
