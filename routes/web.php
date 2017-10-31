4c7962e4b288f27e7580abc26691ab341efaea43
SU-RU <oooaa0727@gmail.com>
2017-10-31 22:42:24 +0800

    練習 6-1：取得 Model

--- routes/web.php
+++ routes/web.php
@@ -39,8 +39,13 @@ $post‐>update([
 */
 
 \App\Post::destroy(3,5,7);
-
+/*
 $allPosts=\App\Post::all();
 dd($allPosts);
 $featuredPosts=\App\Post::where('is_feature',1)->get();
 dd($featuredPosts);
+*/
+$fourthPost=\App\Post::find(6);
+dd($fourthPost);
+$lastPost=\App\Post::orderBy('id','DESC')‐>first();
+dd($lastPost);
