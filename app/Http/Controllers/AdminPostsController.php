7070d9779f491c063b49548981c3b48dba71cd54
SU-RU <oooaa0727@gmail.com>
2017-11-25 17:08:32 +0800

    練習 5：新增資料->頁面跳轉

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -29,5 +29,6 @@ class AdminPostsController extends Controller
     public function store(Request $request)
     {
         Post::create($request->all());
+        return redirect()->route('admin.posts.index');
     }
 }
