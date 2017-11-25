47ca2d3745ff65129bd5fdb6bb01ec74d017c67b
SU-RU <oooaa0727@gmail.com>
2017-11-25 16:26:33 +0800

    練習 4-2：讓資料有時間序列性、隨機感

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -13,6 +13,7 @@ class PostsTableSeeder extends Seeder
     {
         Post::truncate();
         foreach(range(1,20)as$number){
+            $total=20;
             Post::create([
                 'title'=>'title'.$number,
                 'content'=>'content'.$number,
