92d6f0520bbbf17b1c1a4dcc96af578486203a37
SU-RU <oooaa0727@gmail.com>
2017-11-25 16:46:17 +0800

    練習 2：設定 routes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('/',['as'=>'admin.dashboard.index','uses'=>
+        'AdminDashboardController@index']);
+    Route::get('posts' ,['as'=>'admin.posts.index','uses'
+    =>'AdminPostsController@index']);
+    Route::get('posts/create' ,['as'=>'admin.posts.create','uses'
+    =>'AdminPostsController@create']);
+    Route::get('posts/{id}/edit',['as'=>'admin.posts.edit' ,'uses'
+    =>'AdminPostsController@show']);
+});
+
 Route::get('test', function () {
     /*
     \App\Post::create([
