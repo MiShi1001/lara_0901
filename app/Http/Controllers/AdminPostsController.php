b820eeb63f2b26b452c261178e4d1c2c628750d2
SU-RU <oooaa0727@gmail.com>
2017-11-25 17:38:55 +0800

    練習 7-3：在 PostsController的 destroy內刪除資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -40,4 +40,11 @@ class AdminPostsController extends Controller
             return redirect()->route('admin.posts.index');
         }
 
+        public function destroy($id)
+        {
+            Post::destroy($id);
+            return redirect()->route('admin.posts.index');
+            }
+
+
 }
